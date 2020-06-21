efbb9f016fa118d40d8d0d04107d616eb0bf533b
ayushi07011999 <67162258+ayushi07011999@users.noreply.github.com>
2020-06-21 14:06:38 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.css";
+import Examlist from "./Component/Examlist";
+//import Ques from "./Component/Ques"
+//import Newtry from "./Component/Newtry"
+//import Postrequest  from "./Component/Postrequest"
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <Examlist />
+    </div>
+  );
+}
